2af9b0c0bb1b5eede250620f4a1ea6c38c2d5ecc
Mikhail Spiridonov <Muxa2793@mail.ru>
2020-09-10 00:09:14 +0300

    Исправил замечания

--- 2_if2.py
+++ 2_if2.py
@@ -17,23 +17,20 @@
 
 
 def comparing_strings(str_1, str_2):
-  if not isinstance(str_1, str) or not isinstance(str_1, str):
-    if not isinstance(str_1, str):
-      return 0
-    elif not isinstance(str_2, str):
-      return 0
+  if not isinstance(str_1, str) and not isinstance(str_2, str):
+    return 0
 
   elif str_1 == str_2:
     return 1
 
   elif len(str_1) > len(str_2) and str_1 != str_2:
-    if 'learn' in str_2:
+    if 'learn' == str_2:
       return 3
     else:
       return 2
 
 def main():
-  print(comparing_strings(12, '12'))
+  print(comparing_strings(12, 22))
   print(comparing_strings('Это строка', 'Это строка'))
   print(comparing_strings('Эта строка длинее', 'Этой строки'))
   print(comparing_strings('Python', 'learn'))
