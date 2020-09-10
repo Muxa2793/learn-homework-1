bbc6eda87d0577e40711877ce1151c36a0049fa2
Mikhail Spiridonov <Muxa2793@mail.ru>
2020-09-10 21:42:28 +0300

    высылаю Задание с обработкой исключений № 1

--- 6_exception1.py
+++ 6_exception1.py
@@ -11,10 +11,33 @@
 """
 
 def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+  
+  ASK_QUESTION = {
+    'как дела': 'Хорошо',
+    'что делаешь': 'Программирую'
+  }
+  
+  while True:
+    try:
+      user_say = input('Программа: Как дела?\nПользователь: ')
+      user_say = user_say.lower()
+      if user_say == 'хорошо':     
+        print(
+          'Программа: Это отлично! Спроси меня что-нибудь.',
+          'Например: "Как дела?" или "Что делаешь?"',
+          '\nЧтобы закончить нажми "Ctrl+с".'
+        )
+        while True:
+          user_say = input('Пользователь: ')
+          user_say = user_say.lower()
+          if 'как дела' in user_say:
+            print(f"Программа: {ASK_QUESTION['как дела']}\nПрограмма: Спроси ещё что-нибудь")
+          elif 'что делаешь' in user_say:
+            print(f"Программа: {ASK_QUESTION['что делаешь']}\nПрограмма: Спроси ещё что-нибудь")
+    except KeyboardInterrupt:
+      print('\nПока!')
+      break
+  
     
 if __name__ == "__main__":
     ask_user()
